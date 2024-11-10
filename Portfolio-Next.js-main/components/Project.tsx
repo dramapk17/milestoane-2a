c8757cf6a0594a357850d118e962fb51806a4edb
dramapk17 <33402357+dramapk17@users.noreply.github.com>
2024-11-10 18:59:45 +0500

    Add files via upload

---
+++ Portfolio-Next.js-main/components/Project.tsx
@@ -0,0 +1,37 @@
+import "boxicons/css/boxicons.min.css";
+
+const Project1 = () => {
+  return (
+    <div id="projects">
+    <div id="project project-1">
+      <h3>Resume Builder <i className="bx bxs-spreadsheet"></i></h3>
+      <p>A responsive Resume Builder website built with HTML, CSS, and TypeScript.</p>
+      <a href="https://shareable-resume-builder-by-" target="_blank" rel="noopener noreferrer" id="project-link">View Project</a>
+    </div>
+  
+    <div id="project project-2">
+      <h3>Age Calculator <i className="bx bx-calculator"></i></h3>
+      <p>A web application to calculate your exact age in years, months, and days.</p>
+      <a href="https://age-calculator/" target="_blank" rel="noopener noreferrer" id="project-link">View Project</a>
+    </div>
+  
+    <div id="project project-3">
+      <h3>I&apos;D Card Generator <i className="bx bx-id-card"></i></h3>
+      <p>Generate and download your personal ID card. Built using HTML, CSS, and JavaScript.</p>
+      <a href="https:///" target="_blank" rel="noopener noreferrer" id="project-link">View Project</a>
+    </div>
+  
+    <div id="project project-4">
+      <h3>Currency Converter <i className="bx bxl-bitcoin"></i></h3>
+      <p>A simple Currency Converter app to convert your amount built using JavaScript.</p>
+      <a href="https:///" target="_blank" rel="noopener noreferrer" id="project-link">View Project</a>
+    </div>
+  
+    
+  
+    
+  </div>
+  )
+}
+
+export default Project1 
